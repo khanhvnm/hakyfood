8d7e18bc540d582b1fccb6a21f6995c2b4db3847
namkhanhs.dev <namkhanhs.dev@gmail.com>
2026-06-27 22:30:46 +0700

    chore(project): initialize project

---
+++ hakyfood-backend/src/test/java/org/example/hakyfoodbackend/HakyfoodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hakyfoodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HakyfoodBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
