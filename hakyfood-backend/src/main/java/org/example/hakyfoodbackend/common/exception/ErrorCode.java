823329dc3adb56ddcc29f5e9d4de912496f19248
namkhanhs.dev <namkhanhs.dev@gmail.com>
2026-06-28 15:45:23 +0700

    chore(exception): add AppException and ErrorCode for error handling

---
+++ hakyfood-backend/src/main/java/org/example/hakyfoodbackend/common/exception/ErrorCode.java
@@ -0,0 +1,18 @@
+package org.example.hakyfoodbackend.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+
+
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+}
